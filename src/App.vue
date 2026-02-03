84a75624ed3fe6df3cec498e27035c2bc5fa0ad3
Eythan Tournant <144635214+ThatPlas@users.noreply.github.com>
2026-02-03 20:40:34 +0100

    Ajout d'icônes*
    *Ces icônes ont des chances d'être temporaires

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 </script>
 
 <template>
       <nav id="mainMenu">
-
         <RouterLink to="/"><img src="@/assets/img/logo.png"></RouterLink>
-        <RouterLink to="/cartes">Cartes</RouterLink>
-        <RouterLink to="/decks">Decks</RouterLink>
+        <RouterLink to="/cartes"><FontAwesomeIcon class="menuIcon" icon="fa-solid fa-square" size="xl" style="transform: scale(.8,1.1)"/>Cartes</RouterLink>
+        <RouterLink to="/decks"><FontAwesomeIcon class="menuIcon" icon="fa-solid fa-box" size="xl"/>Decks</RouterLink>
       </nav>
   <RouterView />
 </template>
