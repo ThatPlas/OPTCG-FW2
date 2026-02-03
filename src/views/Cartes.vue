602c46819aa51e2518df58d8f6465d1e1f3a3d98
Eythan Tournant <144635214+ThatPlas@users.noreply.github.com>
2026-02-03 20:40:48 +0100

    Vue de test de cartes

--- src/views/Cartes.vue
+++ src/views/Cartes.vue
@@ -1,8 +1,34 @@
 <script setup>
+
+const cartes = [
+    {nom: "Loufy", couleur: "Rouge", puissance:5000, rarete:'SR'},
+    {nom: "Sans jean", couleur: "Rouge", puissance:5000, rarete:'R'},
+    {nom: "Robin", couleur: "Rouge", puissance:5000, rarete:'SSR'},
+    {nom: "Boku wa Doctaaa", couleur: "Rouge", puissance:5000, rarete:'TR'},
+    {nom: "Zoro", couleur: "Rouge", puissance:5000, rarete:'L'},
+]
+
 </script>
 
 <template>
-    cartes
+    <div id="cartesContainer">
+        <div v-for="carte in cartes">
+            <img v-if="carte.rarete == 'L'" class="cardImg" src="@/assets/img/back-leader.webp">
+            <img v-else class="cardImg" src="@/assets/img/back-character.webp"><br>
+            {{ carte.nom }}
+        </div>
+    </div>
 </template>
 
-<style></style>
+<style>
+
+#cartesContainer {
+    display:grid;
+    grid-template-columns: repeat(5, 1fr);
+}
+
+.cardImg {
+  width:200px;
+}
+
+</style>
