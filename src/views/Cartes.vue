51198fba2d265f583a7cf7abb2ee9e0cd3b86ead
Eythan Tournant <144635214+ThatPlas@users.noreply.github.com>
2026-02-04 23:32:36 +0100

    Début des calls API
    Calls API sur les cartes et extensions

--- src/views/Cartes.vue
+++ src/views/Cartes.vue
@@ -1,21 +1,43 @@
 <script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
 
-const cartes = [
-    {nom: "Loufy", couleur: "Rouge", puissance:5000, rarete:'SR'},
-    {nom: "Sans jean", couleur: "Rouge", puissance:5000, rarete:'R'},
-    {nom: "Robin", couleur: "Rouge", puissance:5000, rarete:'SSR'},
-    {nom: "Boku wa Doctaaa", couleur: "Rouge", puissance:5000, rarete:'TR'},
-    {nom: "Zoro", couleur: "Rouge", puissance:5000, rarete:'L'},
-]
+const raretes = ["Common", "Rare", "SuperRare","SecretRare","Special", "Leader"]
+
+// Appel API de toutes les cartes d'une extension
+const listeCartes = ref();
+async function extensionCall(extensionID) {
+    const requeteCartes = await axios.get(`https://raw.githubusercontent.com/buhbbl/punk-records/main/french/data/${extensionID}.json`)
+    listeCartes.value = requeteCartes.data;
+};
+
+// Appel API de toutes les extensions disponibles
+const listeExtensions = ref()
+async function extensionListCall() {
+    const requeteExtensions = await axios.get("https://raw.githubusercontent.com/buhbbl/punk-records/main/french/packs.json")
+    listeExtensions.value = requeteExtensions.data
+};
+
+    onMounted(async () => {
+        await extensionListCall()
+        await extensionCall(622302);
+  })
 
 </script>
 
 <template>
+    <div>
+        <select @change="console.log('changement d\'extension'); extensionCall(extensionID)" v-model="extensionID">
+            <option v-for="extension in listeExtensions" :value="extension.id">{{ extension.raw_title }}</option>
+        </select>
+        <select v-model="rareteFiltre">
+            <option value="*" default>Tous</option>
+            <option v-for="rarete in raretes" :value="rarete">{{rarete}}</option>
+        </select>
+    </div>
     <div id="cartesContainer">
-        <div v-for="carte in cartes">
-            <img v-if="carte.rarete == 'L'" class="cardImg" src="@/assets/img/back-leader.webp">
-            <img v-else class="cardImg" src="@/assets/img/back-character.webp"><br>
-            {{ carte.nom }}
+        <div v-for="carte in listeCartes" class="cardComponent">
+            <img class="cardImg" :src="carte.img_full_url" :note="carte.rarity">
         </div>
     </div>
 </template>
@@ -24,11 +46,17 @@ const cartes = [
 
 #cartesContainer {
     display:grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(7, 1fr);
 }
 
 .cardImg {
   width:200px;
+}
+.cardComponent {
+    display:block;
+}
+.cardComponent.visible {
+    display:none;
 }
 
 </style>
