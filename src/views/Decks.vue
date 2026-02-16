85160b49d3dcf0cb3786dad0c5e04eceb4c0e3ba
Eythan Tournant <144635214+ThatPlas@users.noreply.github.com>
2026-02-17 00:29:50 +0100

    Bases d'une vue de deck
    La page a été agrémentée des bases pour visualiser une structure de deck

--- src/views/Decks.vue
+++ src/views/Decks.vue
@@ -1,8 +1,62 @@
 <script setup>
+import {useRouter} from 'vue-router';
+
+const router = useRouter();
+
+const decks = [
+    {id:1, nom:"fils de pute alias Yamato", description:"AAAAAAAAAAAAAAAAAAAAAAAAAAAAA LA CONNEXION", couleur:"jaune"}
+]
+
+const deckClick = (deckID) => {
+router.push({
+    name:"DeckView",
+    params: {deckID},
+})
+}
+
 </script>
 
 <template>
-    decks
+    <h1>Vos decks</h1>
+    <div id="decksContainer">
+        <a v-for="deck in decks" @click="deckClick(deck.id)">
+            <div class="deckDiv">
+                <img class="cardImg" src="@/assets/img/back-character.webp">
+                <span class="deckName">{{ deck.nom }}</span>
+                <p class="deckDesc">{{ deck.description }}</p>
+            </div>
+        </a>
+    </div>
 </template>
 
-<style></style>
+<style scoped>
+#decksContainer {
+    display:grid;
+    grid-template-columns: repeat(8,1fr);
+}
+
+a:has(div.deckDiv) {
+    margin:10px;
+    text-decoration: none;
+    color:white;
+    text-align: center;
+    cursor:pointer;
+}
+
+.deckDiv {
+    background-color:#313131;
+    border-radius: 25px;
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    padding:25px;
+}
+
+.deckName {
+    font-weight: bold;
+}
+.deckDesc {
+    color:rgb(160, 160, 160);
+}
+
+</style>
