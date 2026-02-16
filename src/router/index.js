327cee96668c8029b9730f42e5aa3d8d40503d3d
Eythan Tournant <144635214+ThatPlas@users.noreply.github.com>
2026-02-17 00:28:20 +0100

    Ajout de routes spécifiques
    Des routes spécifiques, tel qu'inspecteur un deck ou une carte spécifique, ont été ajoutés

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Accueil from '../views/Accueil.vue'
-import Cartes from '../views/Cartes.vue'
-import Decks from '../views/Decks.vue'
+import Accueil from '@/views/Accueil.vue'
+import Cartes from '@/views/Cartes.vue'
+import CarteView from '@/views/CarteView.vue'
+import Decks from '@/views/Decks.vue'
+import DeckView from '@/views/DeckView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {path:'/', name:'Accueil', component:Accueil},
     {path:'/cartes', name:'Cartes', component:Cartes},
+    {path:'/carte/:cardID', name:'CarteView', component:CarteView},
     {path:'/decks', name:'Decks', component:Decks},
+    {path:'/deck/:deckID', name:'DeckView', component:DeckView},
   ],
 })
 
