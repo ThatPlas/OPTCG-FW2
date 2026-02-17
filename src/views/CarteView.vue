2fb0ee01ef9ce5ca98733eaa452df255bc7d5e99
Eythan Tournant <eythan.tournant@gmail.com>
2026-02-17 18:57:38 +0100

    Affichage plus détaillé des infos de carte
    La description de la carte, son coût, sa puissance et son nom ont été rajoutés, transformant les labels avec leur style respectif

--- src/views/CarteView.vue
+++ src/views/CarteView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import axios from 'axios'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute();
@@ -9,13 +9,13 @@ const cardID = route.params.cardID;
 
 
 // Appel API de toutes les cartes d'une extension
-const infosCarte = ref();
+const infosCarte = ref({effect:'', img_full_url: '', trigger:''});
 async function cardCall(cardID) {
     const requeteCoordsCarte = await axios.get(`https://raw.githubusercontent.com/buhbbl/punk-records/main/french/index/cards_by_id.json`)
     const cardQueryCoords = requeteCoordsCarte.data[cardID];
-    console.log(cardQueryCoords)
     const requeteInfosCarte = await axios.get(`https://raw.githubusercontent.com/buhbbl/punk-records/main/french/cards/${cardQueryCoords.pack_id}/${cardID}.json`)
     infosCarte.value = requeteInfosCarte.data
+    console.log(infosCarte.value)
 };
 
 
@@ -23,10 +23,74 @@ async function cardCall(cardID) {
         await cardCall(cardID)
   })
 
+function replaceLabels(oldString) {
+    let newString = oldString
+    if(oldString != null) {
+        // Main labels
+        newString = newString.replace("[Principale]","<span class='label mainLabel'>Principale</span>")
+        newString = newString.replace("[Jouée]","<span class='label mainLabel'>Jouée</span>")
+        newString = newString.replace("[En attaquant]","<span class='label mainLabel'>En attaquant</span>")
+
+        // Warning labels
+        newString = newString.replace("[Contre] ", "<span class='label warningLabel'><i class='counter'></i>Contre</span>")
+
+        //Attribute labels
+        newString = newString.replace("[Bloqueur] ", "<span class='label attributeLabel'>Bloqueur</span>")
+        newString = newString.replace("[Initiative]","<span class='label attributeLabel'>Initiative</span>")
+
+        // Exceptions
+        newString = newString.replace("[Déclenchement] ", "<span class='label triggerLabel'>Déclenchement</span>")
+        newString = newString.replace("[Une fois par tour] ", "<span class='label oncePerTurnLabel'>Une fois par tour</span>")
+        newString = newString.replace("&nbsp;", " ")
+    }
+    return newString
+}
+
+const formattedName = computed(() => replaceLabels(infosCarte.value.name))
+const formattedEffect = computed(() => replaceLabels(infosCarte.value.effect))
+const formattedTrigger = computed(() => replaceLabels(infosCarte.value.trigger))
+
 </script>
 
 <template>
-    {{ infosCarte }}
+    <div id="mainInfos">
+        <img class="card" :src="infosCarte.img_full_url">
+        <div id="textInfos">
+            <h1>{{ formattedName }}</h1>
+            <h2 class="floatingText top right" style="top:-40px">{{ infosCarte.power }}</h2>
+            <h2 class="floatingText top left" style="top:-40px">{{ infosCarte.cost }}</h2>
+            <p id="types" class="label"><span v-for="type in infosCarte.types">{{ type }} <span v-if="type != infosCarte.types[infosCarte.types.length-1]"> / </span> </span></p>
+            <p v-html="formattedEffect"></p>
+            <p v-html="formattedTrigger" v-if="infosCarte.trigger != ''"></p>
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#mainInfos {
+    display:flex;
+    /* align-items: center; */
+    justify-content: space-around;
+    padding:20px;
+}
+#mainInfos h1 {
+    text-align:center;
+}
+#mainInfos p {
+    margin:5px 0;
+}
+
+#textInfos {
+    padding:5px;
+}
+#textInfos #types {
+    background-color:lightgray;
+    color:slategray;
+    width:fit-content;
+    margin:0 auto 5px auto;
+}
+
+.card {
+    height:500px;
+}
+</style>
